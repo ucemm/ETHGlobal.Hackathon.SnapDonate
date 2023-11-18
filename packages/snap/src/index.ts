61b1785d8f294e3f34f1131a9e6238b36f2bb1de
umur <umurcemakbas@gmail.com>
2023-11-18 20:23:27 +0300

    QR Code Generation & UI Adjustments
    SVG type QR code generator added, custom UI MetaMask alert designed.

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,5 @@
 import type { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
+import { panel, text, image, heading } from '@metamask/snaps-ui';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -11,6 +11,40 @@ import { panel, text } from '@metamask/snaps-ui';
  * @returns The result of `snap_dialog`.
  * @throws If the request method is not valid for this snap.
  */
+import QRCode from 'qrcode'; // Import QR Code library
+//import { QRCode } from "react-qr-svg";
+
+export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
+  switch (request.method) {
+    case 'hello':
+      const ethUrl = "ethereum:0x53Dc0c92380cce50e0C3D9DF625478C3d4069bf3@11155111?value=1e20"
+      try {
+        const qrCodeDataUri = await QRCode.toString(ethUrl, { type: 'svg' });
+        return snap.request({
+          method: 'snap_dialog',
+          params: {
+            type: 'alert',
+            content: panel([
+              heading('Ready to impact lives and biodiversity?'),
+              text('Scan this QR code to donate 1% of your recent 1 ETH transaction.'),
+              text('Support UNICEF’s Venture Fund in their work towards climate action, ' +
+              'zero hunger, and other Sustainable Development Goals.'),
+              image(qrCodeDataUri),
+            ]),
+          },
+        });
+      } catch (err) {
+        throw err;
+        //throw new Error('Failed to generate QR Code.');
+      
+      }
+      break;
+    default:
+      throw new Error('Method not found.');
+  }
+};
+
+/*
 export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
   switch (request.method) {
     case 'hello':
@@ -31,3 +65,4 @@ export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
       throw new Error('Method not found.');
   }
 };
+*/
